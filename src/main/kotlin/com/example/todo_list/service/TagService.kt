f59a353856691686b01687b4edd72f1914aecbb2
kotahorii <khorii.micron@gmail.com>
2022-03-09 22:18:55 +0900

    [update] move file

--- src/main/kotlin/com/example/todo_list/service/TagService.kt
+++ src/main/kotlin/com/example/todo_list/service/TagService.kt
@@ -2,7 +2,9 @@ package com.example.todo_list.service
 
 import com.example.todo_list.entity.Tag
 import com.example.todo_list.repository.TagRepository
+import org.springframework.stereotype.Service
 
+@Service
 class TagService(private val tagRepository: TagRepository) {
     fun findAll(): List<Tag> = tagRepository.findAll()
     fun findById(id: Long) = tagRepository.findById(id)
