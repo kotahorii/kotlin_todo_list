15598a7f7762fdd3fa15f13f15d76b1e52e94e6e
kotahorii <khorii.micron@gmail.com>
2022-03-09 21:51:15 +0900

    [update] service

--- src/test/kotlin/com/example/todo_list/service/TagService.kt
+++ src/test/kotlin/com/example/todo_list/service/TagService.kt
@@ -1,4 +1,11 @@
 package com.example.todo_list.service
 
-class TagService {
+import com.example.todo_list.entity.Tag
+import com.example.todo_list.repository.TagRepository
+
+class TagService(private val tagRepository: TagRepository) {
+    fun findAll(): List<Tag> = tagRepository.findAll()
+    fun findById(id: Long) = tagRepository.findById(id)
+    fun save(tag: Tag) = tagRepository.save(tag)
+    fun delete(id: Long) = tagRepository.deleteById(id)
 }
--- src/test/kotlin/com/example/todo_list/service/TaskService.kt
+++ src/test/kotlin/com/example/todo_list/service/TaskService.kt
@@ -1,4 +1,13 @@
 package com.example.todo_list.service
 
-class TaskService {
+import com.example.todo_list.entity.Task
+import com.example.todo_list.repository.TaskRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TaskService(private val taskRepository: TaskRepository) {
+    fun findAll(): List<Task> = taskRepository.findAll()
+    fun findById(id: Long) = taskRepository.findById(id)
+    fun save(task: Task) = taskRepository.save(task)
+    fun delete(id: Long) = taskRepository.deleteById(id)
 }
