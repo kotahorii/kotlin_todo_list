be27570789906120f922a04c6da30366e2b8ebf8
kotahorii <khorii.micron@gmail.com>
2022-03-09 22:00:21 +0900

    [update] controller

--- src/test/kotlin/com/example/todo_list/controller/TagController.kt
+++ src/test/kotlin/com/example/todo_list/controller/TagController.kt
@@ -1,4 +1,38 @@
 package com.example.todo_list.controller
 
-class TagController {
+import com.example.todo_list.entity.Tag
+import com.example.todo_list.service.TagService
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/tags")
+class TagController(private val tagService: TagService) {
+    @GetMapping("")
+    fun findAll(): List<Tag> {
+        return tagService.findAll()
+    }
+
+    @PostMapping("")
+    fun create(@RequestBody tag: Tag): Tag {
+        tagService.save(tag)
+        return tag
+    }
+
+    @GetMapping("{id}")
+    fun findById(@PathVariable id: Long): Optional<Tag> {
+        return tagService.findById(id)
+    }
+
+    @PutMapping("{id}")
+    fun update(@PathVariable id: Long, @RequestBody tag: Tag): Tag {
+        tagService.save(tag.copy(id = id))
+        return tag.copy(id = id)
+    }
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: Long): String {
+        tagService.delete(id)
+        return "delete complete"
+    }
 }
--- src/test/kotlin/com/example/todo_list/controller/TaskController.kt
+++ src/test/kotlin/com/example/todo_list/controller/TaskController.kt
@@ -1,4 +1,38 @@
 package com.example.todo_list.controller
 
-class TaskController {
+import com.example.todo_list.entity.Task
+import com.example.todo_list.service.TaskService
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/tasks")
+class TaskController(private val taskService: TaskService) {
+    @GetMapping("")
+    fun findAll(): List<Task> {
+        return taskService.findAll()
+    }
+
+    @PostMapping("")
+    fun create(@RequestBody task: Task): Task {
+        taskService.save(task)
+        return task
+    }
+
+    @GetMapping("{id}")
+    fun findById(@PathVariable id: Long): Optional<Task> {
+        return taskService.findById(id)
+    }
+
+    @PutMapping("{id}")
+    fun update(@PathVariable id: Long, @RequestBody task: Task): Task {
+        taskService.save(task.copy(id = id))
+        return task.copy(id = id)
+    }
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: Long): String {
+        taskService.delete(id)
+        return "delete complete"
+    }
 }
